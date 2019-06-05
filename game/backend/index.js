d60bfc1f9012a330a6c9edca47c39c5cd3a27e42
KutaybaKudeh <koutaiba.kk@gmail.com>
2019-06-05 16:48:38 -0700

    socket io

--- game/backend/index.js
+++ game/backend/index.js
@@ -1,7 +1,17 @@
-const express=require('express');
-let port=550;
-const app=express();
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+let port = 550;
 app.use(express.static('public'));
-app.listen(port,()=>{
+http.listen(port, () => {
     console.log(`server is running on port ${port}`);
+});
+io.on('connection', function (player) {
+    console.log(player.id + ' has connected')
+    player.on('disconnect', () => {
+        console.log(player.id + ' has disconnected')
+
+    });
+
 });
